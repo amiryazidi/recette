f6fc96358550e316c4d03503a105970f5849ab13
Amir <Amir.yazidi1@esprit.tn>
2021-12-05 14:01:20 +0100

    AddEntityType

--- src/Entity/Aliment.php
+++ src/Entity/Aliment.php
@@ -83,6 +83,11 @@ class Aliment
      */
     private $updated_at;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Type::class, inversedBy="aliments")
+     */
+    private $type;
+
 
     public function getId(): ?int
     {
@@ -181,6 +186,18 @@ class Aliment
     public function setUpdatedAt(\DateTimeInterface $updated_at): self
     {
         $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(?Type $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
